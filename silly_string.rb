240624d9b6e2d5dba9b9f881c4badee31d448050
mayagriffith <mayaegriffith@gmail.com>
2018-08-06 18:36:57 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -9,7 +9,7 @@ puts "My favorite word is #{your_favorite_word}"
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "MY FAVORITE WORD IS #{your_favorite_word}".upcase
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
@@ -17,7 +17,7 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts lowercased_phrase.downcase
 
 # Challenge 4: Count how many letters are in that big_word
 
@@ -25,7 +25,7 @@ big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #{letter_count} in #{big_word.length}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
